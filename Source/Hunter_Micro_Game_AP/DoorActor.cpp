59c47110d0d766505771b02c402ae14fd8e0ca18
styraselkie <hunterovington1@gmail.com>
2025-04-28 21:47:25 +1000

    updated files

--- Source/Hunter_Micro_Game_AP/DoorActor.cpp
+++ Source/Hunter_Micro_Game_AP/DoorActor.cpp
@@ -8,12 +8,13 @@ ADoorActor::ADoorActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	/*
 	DoorFrame = CreateDefaultSubobject(UStaticMeshComponent > (TEXT("DoorFrame"));
 	DoorFrame->SetupAttachment(RootComponent);
 
 	Door = CreateDefaultSubobject(UStaticMeshComponent > (TEXT("DoorFrame"));
-	Door->SetupAttachment(DoorFrame)l
+	Door->SetupAttachment(DoorFrame);
+	*/
 }
 
 // Called when the game starts or when spawned
